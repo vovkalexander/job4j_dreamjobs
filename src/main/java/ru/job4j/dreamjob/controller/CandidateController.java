8df20229f8a1c569712bc1d9abe48d0306e64fb7
Александр <46612532+vovkalexander@users.noreply.github.com>
2026-06-19 06:46:22 +0300

    HTML form.Page of creating candidate

--- src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
+++ src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
@@ -21,4 +21,9 @@ public class CandidateController {
         return "candidates/list";
     }
 
+    @GetMapping("/create")
+    public String getCreationPage() {
+        return "candidates/create";
+    }
+
 }
